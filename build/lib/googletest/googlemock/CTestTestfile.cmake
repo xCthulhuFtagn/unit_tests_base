fa0b37e7978ed2e2ad1ae23e0dbb07ad8f9a4455
Максим Колодяжный <mkolodyazhny@gmail.com>
2021-10-01 16:22:22 +0300

    yeah

---
+++ build/lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: E:/DEV_Workspaces/From_VStudio/OTUS/unit_testing/lib/googletest/googlemock
+# Build directory: E:/DEV_Workspaces/From_VStudio/OTUS/unit_testing/build/lib/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ build/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/DEV_Workspaces/From_VStudio/OTUS/unit_testing/lib/googletest/googletest
+# Build directory: E:/DEV_Workspaces/From_VStudio/OTUS/unit_testing/build/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
